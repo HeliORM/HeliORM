034f64c8c034ea0912450fc6d00b079373b30327
Gideon Le Grange <gideon@legrange.me>
2019-07-07 15:30:55 +0200

    Small cleanups

--- helicopter-orm/src/main/java/net/legrange/orm/Database.java
+++ helicopter-orm/src/main/java/net/legrange/orm/Database.java
@@ -3,13 +3,25 @@ package net.legrange.orm;
 import java.util.List;
 
 /**
+ * The interface that must be implemented to define a database for use by the
+ * ORM
  *
  * @author gideon
  */
 public interface Database {
 
+    /**
+     * Return the SQL database name.
+     *
+     * @return The SQL name
+     */
     String getSqlDatabase();
 
+    /**
+     * Return the tables associated with this database.
+     *
+     * @return The tables
+     */
     List<Table> getTables();
 
 }
