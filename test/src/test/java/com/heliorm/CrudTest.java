0b506a16d69ee2193d1c35842da6de0f15b3157f
Gideon Le Grange <gideon@legrange.me>
2020-09-17 22:23:02 +0200

    Added first CRUD test

--- test/src/test/java/com/heliorm/CrudTest.java
+++ test/src/test/java/com/heliorm/CrudTest.java
@@ -13,6 +13,7 @@ import test.pets.Pet;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.heliorm.TestData.makeCat;
@@ -32,6 +33,7 @@ public class CrudTest extends AbstractOrmTest {
 
     private static final int MAX_PERSONS = 3;
     private static List<Person> persons;
+    private static List<Cat> cats = new ArrayList<>();
 
     @BeforeAll
     public static void setupData() throws OrmException {
@@ -45,16 +47,26 @@ public class CrudTest extends AbstractOrmTest {
         Person person = persons.get(0);
         Cat cat = makeCat(person);
         Cat saved = orm.create(cat);
+        cats.add(saved);
         assertNotNull(saved, "The object returned by create should not be null");
         assertTrue(cat.getId() == null, "The id of the new object must be null before create");
         assertTrue(saved.getId() != null, "The id of the new object must be not-null after create");
         assertTrue(pojoCompareExcludingKey(cat, saved), "The new and created objects must be the same apart from the key");
     }
 
+    @Test
+    public void testDelete() throws Exception {
+        say("Testing delete with Long key");
+        Cat cat = cats.remove(0);
+        orm.delete(cat);
+        Optional<Cat> opt = orm.select(CAT).where(CAT.id.eq(cat.getId())).optional();
+        assertFalse(opt.isPresent(), "No object for delete id must be in the database");
+    }
+
+
     @AfterAll
     public static void removeData() throws OrmException {
-        List<Pet> pets = orm.select(PET).list();
-        for (Pet pet : pets) {
+        for (Pet pet : cats) {
             orm.delete(pet);
         }
     }
