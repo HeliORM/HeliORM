4c5509501a23d3e6d049e5e2cf8ecca48b4524d7
Gideon Le Grange <gideon@legrange.me>
2024-04-07 23:27:40 +0200

    Tests passing

--- test/src/main/java/com/heliorm/test/place/Province.java
+++ test/src/main/java/com/heliorm/test/place/Province.java
@@ -5,7 +5,26 @@ import com.heliorm.annotation.Pojo;
 import com.heliorm.annotation.PrimaryKey;
 
 @Pojo
-public record Province(@PrimaryKey Long provinceId,
-                       @Column(length = 30) String name) {
-    
+public final class Province {
+
+    @PrimaryKey
+    private Long provinceId;
+    @Column(length = 30)
+    private String name;
+
+    public Province(Long provinceId, String name) {
+        this.provinceId = provinceId;
+        this.name = name;
+    }
+
+    public Long provinceId() {
+        return provinceId;
+    }
+
+    public Province() {
+    }
+
+    public String name() {
+        return name;
+    }
 }
